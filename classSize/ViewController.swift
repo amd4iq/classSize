76380e502a7a43435fe40add698d2daf33f438c8
Ahmed Alwaeli <ahmedalwaeli@Ahmeds-MacBook-Pro.local>
2017-01-20 20:43:03 +0300

    Initial Commit

---
+++ classSize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  classSize
+//
+//  Created by Ahmed Alwaeli on 1/20/17.
+//  Copyright © 2017 Ahmed Alwaeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
